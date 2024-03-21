7f3204f907606dbe0581734f1c734bbc1ca05482
keithowino <designsolutions1629@gmail.com>
2024-03-21 17:18:46 +0300

    Complete lab.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+// ----------------------------------------
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+};
+
+// ----------------------------------------
+
+var bestCustomer;
+
+function setBestCustomer(){
+  bestCustomer = "not bob";
+};
+
+// ----------------------------------------
+
+function overwriteBestCustomer(){
+  bestCustomer = "maybe bob";
+};
+
+// ----------------------------------------
+
+const leastFavoriteCustomer = "";
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 1;
+};
